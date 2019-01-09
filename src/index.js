ca40da667c9c0a7afebb05dc5f37a67445169e1d
Roie Cohen <roie.cohen@gmail.com>
2019-01-09 23:43:06 +0000

    init

--- src/index.js
+++ src/index.js
@@ -2,138 +2,80 @@ const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const faker = require('faker')
 
-const fakeCost = () => {
-  return {
-    currency: faker.random.arrayElement(['USD', 'EUR']),
-    value: faker.fake('{{commerce.price}}')
-  }
-}
-const fakeLocation = () => {
-  return {
-    id: faker.fake('{{random.uuid}}'),
-    name: faker.fake('{{hacker.noun}}'),
-    coordinates: {
-      lat: faker.fake('{{address.latitude}}'),
-      long: faker.fake('{{address.longitude}}')
-    }
-  }
-}
-
-const fakePart = () => ({
-  id: faker.fake('{{random.uuid}}'),
-  name: faker.fake('{{hacker.noun}}'),
-  descriptor: faker.fake('{{random.uuid}}')
-})
-
-const fakeParts = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return fakePart()
-  })
-}
-
-const fakeManufacturer = () => ({
-  id: faker.fake('{{random.uuid}}'),
-  name: faker.fake('{{hacker.noun}}'),
-  location: fakeLocation()
-})
-
-const fakeManufacturers = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return fakeManufacturer()
-  })
-}
-
-let seedParts = fakeParts(2)
-const randomPart = () => {
-  return faker.random.arrayElement(seedParts)
-}
-const randomParts = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return randomPart()
-  })
-}
-
-const fakeOrder = () => ({
-  id: faker.fake('{{random.uuid}}'),
-  part: randomPart(),
-  deliveryDate: new Date(),
-  cost: fakeCost(),
-  status: faker.random.arrayElement(['OPEN', 'PURCHASED', 'ON_ROUTE', 'CLOSE'])
-})
-
-const fakeOrders = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return fakeOrder()
-  })
-}
-let seedOrders = fakeOrders(2)
-
-const randomOrder = () => {
-  return faker.random.arrayElement(seedOrders)
-}
-
-const randomOrders = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return randomOrder()
-  })
-}
-
-let seedManufacturers = fakeManufacturers(2)
-const randomManufacturer = () => {
-  let manufacturer = faker.random.arrayElement(seedManufacturers)
-  manufacturer.parts = randomParts(10)
-  manufacturer.orders = randomOrders(10)
-  return manufacturer
-}
-
-const randomManufacturers = quantity => {
-  return Object.keys([...Object.keys([...Array(quantity)])]).map(x => {
-    return randomManufacturer()
-  })
-}
-
 const resolvers = {
   Query: {
     info() {
-      return 'query'
+      return {
+        name: 'Demo Wrapper',
+        version: '0.0.1'
+      }
+    },
+    getTopCitiesByEvent(parent, { capital }, info, context) {
+      return [
+        {
+          name: 'Seattle'
+        },
+        {
+          name: 'London'
+        },
+        {
+          name: 'New York'
+        }
+      ]
+    },
+    getTopCitiesByCost(parent, { capital }, info, context) {
+      return [
+        {
+          name: 'Seattle'
+        },
+        {
+          name: 'Los Angeles'
+        },
+        {
+          name: 'San Fransico'
+        }
+      ]
     },
 
-    partLocation(partInput) {
-      return fakeLocation()
+    idealCityFromCitiesByEventAndCost(
+      parent,
+      { citiesByEvent, citiesByCost },
+      info,
+      context
+    ) {
+      return {
+        name: 'Seattle'
+      }
     },
-    partCost(partInput) {
-      return fakeCost()
+    capitalToIdealLaunchDate(parent, { Capital }, info, context) {
+      return '01/10/2019'
     },
-    availableManufactureres(partInput) {
-      return randomManufacturers(2).map(m => {
-        return {
-          ...m,
-          parts: m.parts.map(p => ({
-            ...p,
-            order: randomOrder(),
-            manufacturer: m
-          })),
-          orders: m.orders.map(o => ({
-            ...o,
-            supplier: randomManufacturer()
-          }))
+    capitalToRequiredInventory(parent, { Capital }, info, context) {
+      return [
+        {
+          name: 'Pastrami'
         }
-      })
+      ]
     },
-    bestManufacturer(partInput) {
-      let m = randomManufacturer()
-      m.orders = randomOrders(10).map(o => ({ ...o, supplier: m }))
-      m.parts = randomParts(10).map(p => ({
-        ...p,
-        order: randomOrder(),
-        manufacturer: m
-      }))
-      return m
+    generateLaunchStrategy(
+      parent,
+      { idealCity, idealLaunchDate, requiredInventory },
+      info,
+      context
+    ) {
+      return {
+        idealCity,
+        idealLaunchDate,
+        requiredInventory
+      }
     }
   },
   Mutation: {
     info() {
-      return 'mutate'
+      return {
+        name: 'Demo wrapper',
+        version: '0.0.1'
+      }
     }
   }
 }
@@ -145,7 +87,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-      endpoint: 'https://maana-procurement.herokuapp.com/marketplace/dev', // the endpoint of the Prisma API
+      endpoint: 'https://maana-incubator.herokuapp.com/training-demo/dev', // the endpoint of the Prisma API
       debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     })
